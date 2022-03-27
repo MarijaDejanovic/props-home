b2c3e162960b3783a0e855a1e1d9bb60a60a9d48
Marija Dejanovic <dejanovic.marija2801@gmail.com>
2022-03-27 15:02:41 +0200

    last commit

--- props/src/App.js
+++ props/src/App.js
@@ -1,2 +1,28 @@
+import React from 'react';
 import './App.css';
+import { UserFunction, UserClass, UserChildren } from './User';
+
+const users = [
+  {name: 'Pero', age: 21},
+  {name: 'Ana', age: 34},
+  {name: 'Marko', age: 45},
+];
+
+export default class App extends React.Component
+{
+  render(){
+    return(
+      <>
+      <h1>
+        React aplikacija
+      </h1>
+      <UserFunction name={users[0].name} age={users[0].age}/>
+      <UserClass name={users[1].name} age={users[1].age}/>
+      <UserChildren name={users[2].name} age={users[2].age} >
+        I hobi mi je tenis!
+      </UserChildren>
+      </>
+    );
+  }
+}
 
---
+++ props/src/User/UserFunction.js
@@ -0,0 +1,7 @@
+export default function UserFunction ({name, age}){
+    return(
+        <>
+        <p>Pozdrav, moje ime je {name} i imam {age} godina!</p>
+        </>
+    );
+}
